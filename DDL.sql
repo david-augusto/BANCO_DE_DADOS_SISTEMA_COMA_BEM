a78f8e96c6e5f2a3b67d09fa647d5d2a630de95d
David Augusto <davidmaciel178@gmail.com>
2025-05-08 23:54:50 -0300

    Insercao de dados

---
+++ DDL.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE coma_bem;
+
+USE coma_bem;
+
+CREATE TABLE produto(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    preco DECIMAL(10,2) NOT NULL,
+    quantidade DECIMAL(10,2) NOT NULL,
+    unidde_medida VARCHAR(20) NOT NULL
+);
+
+ALTER TABLE produto RENAME COLUMN unidde_medida to unidade_medida;
+
+CREATE TABLE cliente(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    telefone VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE pedido(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    id_cliente INT NOT NULL,
+    data_pedido DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (id_cliente) REFERENCES cliente(id)
+);
+
+CREATE TABLE pedido_produto(
+	id_pedido INT,
+	id_produto INT,
+    quantidade DECIMAL (10,2) NOT NULL,
+    preco_unitario DECIMAL (10,2) NOT NULL,
+    PRIMARY KEY (id_pedido, id_produto),
+    FOREIGN KEY (id_pedido) REFERENCES pedido(id),
+    FOREIGN KEY (id_produto) REFERENCES produto(id)
+
+);
---
+++ DML.sql
@@ -0,0 +1,14 @@
+INSERT INTO produto (nome, preco, quantidade, unidade_medida) VALUES 
+('Carne',30.00,2.00, 'kg'),
+('Óleo', 30.00, 1.00, 'unidade'),
+('Leite', 30.00,1.50, 'litros'),
+('Alface', 30.00, 1.00, 'unidade');
+
+INSERT INTO cliente(nome, email, telefone) VALUES
+('David Augusto', 'david@email.com', '11122244445');
+
+INSERT INTO pedido (id_cliente) VALUES (1);
+
+INSERT INTO pedido_produto (id_pedido, id_produto, quantidade, preco_unitario) VALUES
+(1,1,1.00, 30.00),
+(1,3,2.00, 30.00);
